6f6fb6b6da65729bbde461be37fd2dd98f79e961
VardanKocharyan <vardanqocharyan5>
2026-06-13 18:32:29 +0400

    Adding Knight's Tour

---
+++ BackTracking/Knights_Tour/include/Create_and_run.hpp
@@ -0,0 +1,7 @@
+#pragma once 
+#include "Knights_Tour.hpp"
+
+void create_and_run(int n) {
+    Knights_Tour test(n);
+    test.GoAround();
+}
---
+++ BackTracking/Knights_Tour/src/test.cpp
@@ -0,0 +1,7 @@
+#include "Create_and_run.hpp"
+
+int main() {
+    create_and_run(5);  
+
+  return 0;
+}
